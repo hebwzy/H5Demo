05b82fe7c7d56fd702a355e2e190329b99b6a30f
wangzhiyong <814408834@qq.com>
2020-01-19 19:58:49 +0800

    h5demo对webview进行设置。

--- app/src/main/java/com/xd/h5demo/MainActivity.java
+++ app/src/main/java/com/xd/h5demo/MainActivity.java
@@ -2,9 +2,13 @@ package com.xd.h5demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,48 @@ public class MainActivity extends AppCompatActivity {
         web = findViewById(R.id.web);
         settings= web.getSettings();
         settings.setJavaScriptEnabled(true);
-        web.loadUrl("file:///android_asset/code.html");
+        //扩大比例的缩放
+        settings.setUseWideViewPort(true);
+         //自适应屏幕
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        settings.setLoadWithOverviewMode(true);
+
+
+        settings.setDomStorageEnabled(true);
+        settings.setAppCacheMaxSize(1024*1024*8);
+        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+        settings.setAppCachePath(appCachePath);
+        settings.setAllowFileAccess(true);
+        settings.setAppCacheEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(
+                    WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        }
+
+        web.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return  true;
+            }
+
+
+        });
+
+//        web.loadUrl("file:///android_asset/code.html");
+        web.loadUrl("https://yuedu.163.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack())
+        {
+            web.goBack();
+        }
+        else
+          super.onBackPressed();
     }
 }
